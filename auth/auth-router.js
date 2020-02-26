4c2d79c4b77860e4574a22c60b7d75ed6055dd7c
Devanee Reid <devaneereid@gmail.com>
2020-02-26 12:07:33 -0700

    final notes from lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); //  install the npm package
 
 const Users = require('../users/users-model.js');
+const { jwtSecret } = require('../config/secrets.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,8 +27,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);  // get a token
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token, /// send the token 
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role || 'user',
+  };
+  // const secret = process.env.JWT_SECRET || 'is it secret, is it safe?' // have to use same secret to get the token and verify the token
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, jwtSecret, options)
+};
 
 module.exports = router;
